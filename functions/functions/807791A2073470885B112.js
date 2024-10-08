20876eb41efbcb2186b94b2719bc9b4275bd7def
Rais Alam <rais@cloudeagle.ai>
2024-10-08 18:42:53 +0530

    Added fucntion

--- functions/functions/807791A2073470885B112.js
+++ functions/functions/807791A2073470885B112.js
@@ -1,7 +1,5 @@
 export function onRequest(context) {
-  const jsonResponse = {
-    result:{"error":"Username and Password Incorrect | Join Telegram @PhuongDoxD"}
-  };
+  const jsonResponse = {"error":"Username and Password Incorrect | Join Telegram @PhuongDoxD"};
 
   return new Response(JSON.stringify(jsonResponse), {
     status: 200,
