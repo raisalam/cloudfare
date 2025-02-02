6ddb2f652f41d8b2853e878bbfa930ea690fbf6b
raisalam <rais7jmi@gmail.com>
2025-02-02 12:02:35 +0530

    Update oOooOHID.js

--- functions/functions/oOooOHID.js
+++ functions/functions/oOooOHID.js
@@ -1,7 +1,7 @@
 export function onRequest(context) {
-  const jsonResponse ={"result":{"\u001bU\u000bF\u0005R\u0006":"\u0005E\u001bV\u0001F\u0010","\u0005D\u0019A\u0011F":"D\u0000H"}};
+  const jsonResponse ='{"result":{"\u001bU\u000bF\u0005R\u0006":"\u0005E\u001bV\u0001F\u0010","\u0005D\u0019A\u0011F":"D\u0000H"}}';
 
-  return new Response(JSON.stringify(jsonResponse), {
+  return new Response(jsonResponse, {
     status: 200,
     headers: {
       "content-type": "application/json"
